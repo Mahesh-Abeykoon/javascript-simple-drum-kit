92871088b943fd9be48d1fd9840e25f66947b050
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-05-19 11:21:35 +0530

    Initial first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var drumButtonImages = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i < drumButtonImages; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", something);
+        function something (){
+            // alert('clicked')
+    }
+}
